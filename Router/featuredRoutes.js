8ea1c472585e611b20ffca7629697689546f3c37
ismail-is <ismailthalithanooji@gmail.com>
2026-03-28 11:03:52 +0530

    test 11

--- Router/featuredRoutes.js
+++ Router/featuredRoutes.js
@@ -3,7 +3,7 @@ const { createFeaturedProduct, getAllFeaturedProducts, getSingleFeaturedProduct,
 const router = express.Router();
 
 
-router.post('/insertFeaturedProductsss',createFeaturedProduct);
+router.post('/insertFeaturedProduct',createFeaturedProduct);
 router.get('/allFeaturedProducts',getAllFeaturedProducts);
 router.get('/allFeaturedProducts/:id',getSingleFeaturedProduct);
 router.put('/FeaturedProductUpdate/:id',updateFeaturedProduct);
